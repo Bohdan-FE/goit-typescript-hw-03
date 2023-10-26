50819a27b8764385a649dbfa507fd612a449f7f4
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-10-26 22:53:34 +0200

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,62 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  
+  constructor(key: Key) {
+    this.key = key;
+    }
+    
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+
+    abstract openDoor(key: Key): void;;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    } else {
+      console.log('Invalid key.');
+    }
+  }
+}
+
+
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
